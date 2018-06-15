1f902b760aae6590ec6b86454eaea8d88349a072
Jeffrey Eapen <j.eapen10@gmail.com>
2018-06-14 21:34:36 -0400

    Added changes to js code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
+var topics = ["The Office", "Arrested Development", "Seinfeld", "Parks and Recreation", "Friends", "TheFresh Prince of Bel-Air"];
 
-    var topics = ["The Office", "Arrested Development", "Seinfeld", "Parks and Recreation", "Friends", "TheFresh Prince of Bel-Air"];
+
+function displayShows() {
+
+    var show = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=viY2ZvKbfUqJapQzLLEj13V9sSxgXKYJ&limit=10";
     
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    }).done(function(response) {
+        // Performs this function when the request is recieved back from the API
+            var results = response.data;
+            console.log(response);
+    
+        // Create variables for the rating of gifs
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+    
+        // Creates a variable for each image
+        var showImage = $("<img>");
+        // Create variables for static and animated gifs
+    
+        }        
+    }
+
+
+
     function renderButtons() {
 
         // Empty the shows the div so that new selections will get added to an empty div
@@ -22,34 +48,11 @@ $(document).ready(function() {
 
         };
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-
-    }).done(function(response) {
-    // Performs this function when the request is recieved back from the API
-        var results = response.data;
-        console.log(response);
-
-    // Create variables for the rating of gifs
-    var rating = results[i].rating;
-    var p = $("<p>").text("Rating: " + rating);
-
-    // Creates a variable for each image
-    var showImage = $("<img>");
-    // Create variables for static and animated gifs
-
-
-    }        
-
-   
-    var show = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=viY2ZvKbfUqJapQzLLEj13V9sSxgXKYJ&limit=10";
     
-   
-    }
-}
 
+    
+
+    }
 
 });
 
